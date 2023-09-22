d300e3f18de8bc27e55643a57e7af04e039baa1d
dardirii <dimasdardiri@gmail.com>
2023-09-23 00:46:24 +0700

    API Product Clear

--- app/product/controller.js
+++ app/product/controller.js
@@ -58,6 +58,73 @@ const store = async(req, res, next) => {
     }
 }
 
+const update = async(req, res, next) => {
+    try{
+        let payload = req.body
+        let { id } = req.params;
+
+        if(req.file){
+            let tmp_path = req.file.path;
+            let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+            let filename = req.file.filename + '.' + originalExt;
+            let target_path = path.resolve(config.rootPath, `public/images/products/${filename}`);
+
+            const src = fs.createReadStream(tmp_path);
+            const dest = fs.createWriteStream(target_path);
+            src.pipe(dest)
+
+            src.on('end', async () => {
+                try {
+                    
+                    let product = await Product.findById(id);
+                    let currentImage = `${config.rootPath}/public/images/products/${products.image_url}`
+
+                    if(fs.existsSync(currentImage)){
+                        fs.unlinkSync(currentImage)
+                    }
+                    product = await Product.findByIdAndUpdate(id, payload, {
+                        new: true,
+                        runValidators: true
+                    })
+                    return res.json(product);
+
+                } catch(err){
+                    fs.unlinkSync(target_path);
+                    if (err && err.name === 'ValidationError'){
+                        return res.json({
+                            error: 1,
+                            message: err.message,
+                            fields: err.errors
+                        })
+                    }
+                    next(err);
+
+                }
+            });
+
+            src.on('error', async() => {
+                next(err)
+            });
+        } else {
+
+            let product = await Product.findByIdAndUpdate(id, payload, {
+                new: true,
+                runValidators: true
+            })
+            return res.json(product);
+        }
+    } catch(err) {
+        if (err && err.message === 'ValidationError'){
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            })
+        }
+        next(err);
+    }
+}
+
 const index = async(req, res, next) => {
     try {
         let { skip = 0, limit = 10 } = req.query;
@@ -72,7 +139,23 @@ const index = async(req, res, next) => {
     }
 }
 
+const destroy = async(req, res, next)=> {
+    try {
+        let product = await Product.findOneAndDelete(req.params.id);
+        let currentImage = `${config.rootPath}/public/images/products/${products.image_url}`
+
+        if(fs.existsSync(currentImage)){
+            fs.unlinkSync(currentImage)
+        }
+        return res.json(product)
+    }catch(err){
+        next(err)
+    }
+}
+
 module.exports = {
     store,
-    index
+    update,
+    index,
+    destroy
 }
